b57ef677a7c9460031476d189bdc36a99b278c68
emmanuel irandi <isaacirandi6@gmail.com>
2024-11-25 11:31:44 +0000

    Page linking setup

--- une-frontend/src/App.jsx
+++ une-frontend/src/App.jsx
@@ -1,11 +1,19 @@
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
+import WelcomePage from './components/WelcomePage'
+import GamePage from './components/GamePage'
+import GameOverPage from './components/GameOverPage'
 
 function App() {
 
   return (
     <>
       <h1>UNO placeholder</h1>
+      <Routes>
+        <Route path="/" element={<WelcomePage/>}/>
+        <Route path="/Game" element={<GamePage/>}/>
+        <Route path="/GameOver" element={<GameOverPage/>}/>
+      </Routes>
     </>
   )
 }
---
+++ une-frontend/src/components/GameOverPage.jsx
@@ -0,0 +1,11 @@
+import {Link} from "react-router-dom"
+
+export default function GameOverPage(){
+    return (
+        <>
+        <Link to="/">
+        Game over page
+        </Link>
+        </>
+    )
+}
