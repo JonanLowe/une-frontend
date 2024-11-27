59f5fb91761e6693080e9bd0394332677026c45a
Cody2612 <b.cody_26@yahoo.com>
2024-11-27 09:49:07 +0000

    add partial styling, use props and functionality

--- une-frontend/src/pages/Player2.jsx
+++ une-frontend/src/pages/Player2.jsx
@@ -1,17 +1,21 @@
-export default function Player2() {
+export default function Player2({ hand, isCurrentPlayer, onPlayCard }) {
   /* Top Player */
   return (
         <div className="position-absolute top-0 start-50 translate-middle-x bg-light p-3 rounded w-50 text-center">
           <div className="fw-bold mb-2">Player 2</div>
           <div className="d-flex justify-content-center gap-2">
-            {[...Array(7)].map((_, i) => (
-              <div
-                key={i}
-                className="bg-white border border-secondary rounded shadow-sm"
-                style={{ width: '100px', height: '150px' }}
-              ></div>
-            ))}
-          </div>
+          {hand.map((card, i) => (
+            <div
+              key={i}
+              className="bg-white border border-secondary rounded shadow-sm"
+              style={{ width: '100px', height: '150px', backgroundColor: card.cardColour, cursor: isCurrentPlayer ? 'pointer' : 'default' }}
+          onClick={() => isCurrentPlayer && onPlayCard(i)}
+        >
+          <p>{card.cardNumber}</p>
+          {card.cardColour}
         </div>
+          ))}
+    </div>
+  </div>
   )
 }
