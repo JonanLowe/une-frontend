67841c4bc4aa592236040284e4f406916f79eb19
Cody2612 <b.cody_26@yahoo.com>
2024-11-27 09:45:37 +0000

    add initialization of the game and get disabled afterwards

--- une-frontend/src/pages/CallUnoButton.jsx
+++ une-frontend/src/pages/CallUnoButton.jsx
@@ -1,7 +1,12 @@
-export default function CallUnoButton() {
+export default function CallUnoButton({ onClick, disabled }) {
   return (
-      <button className="btn btn-primary position-absolute bottom-0 end-0 m-3" style={{zIndex:1000, width: '200px', height: '200px'}}>
-        Call UNO !
+      <button 
+      className="btn btn-primary position-absolute bottom-0 end-0 m-3" 
+      style={{zIndex:1000, width: '200px', height: '200px'}}
+      onClick={onClick}
+      disabled={disabled}
+      >
+        {disabled ? "Game In Progress" : "Start Game"}
       </button> 
   )
 }
