ae37ab5de070df34c26ad55d1173ea46503c9c16
Cody2612 <b.cody_26@yahoo.com>
2024-11-27 09:54:22 +0000

    passed down props and functions to the deck and played card area

--- une-frontend/src/pages/PilesArea.jsx
+++ une-frontend/src/pages/PilesArea.jsx
@@ -1,11 +1,16 @@
 import DeckArea from "./DeckArea";
 import DiscardArea from "./DiscardArea";
 
-export default function PilesArea() {
+export default function PilesArea({ deck, discardPile, currentPlayer, onDrawCard, hasValidMove }) {
   return (
     <div className="position-absolute top-50 start-50 translate-middle bg-warning p-3 rounded d-flex gap-3">
-        <DeckArea />
-        <DiscardArea />
+      <DeckArea
+        deck={deck}
+        currentPlayer={currentPlayer}
+        onDrawCard={onDrawCard}
+        hasValidMove={hasValidMove}
+      />
+      <DiscardArea discardPile={discardPile} />
     </div>
-  )
+  );
 }
